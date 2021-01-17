0c3afbb282174891ea173aa7a9c7fed8a4a478c5
bogdan <bogdanbobletec@gmail.com>
2021-01-17 08:47:20 -0500

    setup apollo client

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,7 +1,17 @@
 import type { AppProps } from "next/app";
+import Meta from "../components/Meta";
+import { ApolloProvider } from "@apollo/react-hooks";
+import { useApollo } from "../lib/apolloClient";
 
-const App = ({ Component, pageProps }: AppProps) => (
-  <Component {...pageProps} />
-);
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  const apolloClient = useApollo(pageProps.initialApolloState);
+console.log({apolloClient})
+  return (
+    <ApolloProvider client={apolloClient}>
+      <Meta />
+      <Component {...pageProps} />
+    </ApolloProvider>
+  );
+};
 
 export default App;
