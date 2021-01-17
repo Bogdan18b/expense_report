3f2226787ea327e5adc481d3b6aa6702dc690657
bogdan <bogdanbobletec@gmail.com>
2021-01-17 09:57:48 -0500

    add sign up form

---
+++ frontend/components/SignUp.tsx
@@ -0,0 +1,57 @@
+import {useState} from 'react';
+import gql from "graphql-tag";
+import {useMutation} from '@apollo/react-hooks';
+
+const SIGN_UP_MUTATION = gql`
+  mutation SIGN_UP_MUTATION($email: String!, $name: String!, $password: String!) {
+    signUp(email: $email, name: $name, password: $password) {
+      id
+      email
+      name
+    }
+  }
+`;
+
+const SignUp: React.FC = () => {
+  const [name, setName] = useState('')
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [signUp, {data}] = useMutation(SIGN_UP_MUTATION)
+
+  const updateName = e => setName(e.target.value)
+  const updateEmail = e => setEmail(e.target.value)
+  const updatePassword = e => setPassword(e.target.value)
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    console.log({name, email, password})
+    signUp({variables: {name, email, password}})
+    console.log({data})
+
+  }
+
+  const isSubmitDisabled = [name, email, password].includes('');
+
+  return (
+    <form>
+      <fieldset>
+        <h2>Sign Up</h2>
+        <label htmlFor="name">
+          Name
+          <input type="text" name="name" value={name} onChange={updateName}/>
+        </label>
+        <label htmlFor="email">
+          Email
+          <input type="text" name="email" value={email} onChange={updateEmail}/>
+        </label>
+        <label htmlFor="password">
+          Password
+          <input type="password" name="password" value={password} onChange={updatePassword}/>
+        </label>
+        <button type="submit" disabled={isSubmitDisabled} onClick={handleSubmit}>Sign Up</button>
+      </fieldset>
+    </form>
+  );
+};
+
+export default SignUp;
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,3 +1,9 @@
-const Home: React.FC = () => <p>home</p>;
+import styled from "styled-components";
+import gql from "graphql-tag";
+import SignUp from "../components/SignUp";
+
+const Home: React.FC = () => {
+  return <SignUp />;
+};
 
 export default Home;
