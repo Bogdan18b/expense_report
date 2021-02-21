e23f6706ff3cf2fb1c9ba1ce52bc8a252fca9e08
bogdan <bogdanbobletec@gmail.com>
2021-02-21 06:11:35 -0500

    update schema

--- backend/src/resolvers/Mutation.js
+++ backend/src/resolvers/Mutation.js
@@ -1,5 +1,10 @@
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const {randomBytes} = require('crypto');
+const {promisify} = require('util');
+const {transport, sendEmail} = require('../mail');
+
+const maxAge = 1000 * 60 * 60 * 24 * 365;
 
 const Mutation = {
   async signUp(parent, args, ctx, info) {
@@ -22,7 +27,7 @@ const Mutation = {
     const {email, password} = args;
     const user = await ctx.db.query.user({where: {email}})
     if (!user) {
-      throw new Error(`no user found with email${email}`);
+      throw new Error(`no user found with email ${email}`);
     }
     const valid = await bcrypt.compare(password, user.password);
     if (!valid) {
@@ -31,7 +36,58 @@ const Mutation = {
     const token = jwt.sign({userId: user.id}, process.env.APP_SECRET);
     ctx.response.cookie('token', token, {
       httpOnly: true,
-      maxAge: 1000 * 60 * 60 * 24 * 365,
+      maxAge,
+    });
+    return user;
+  },
+  async signOut(parent, args, ctx, info) {
+    ctx.response.clearCookie('token');
+    return {message: 'You have signed out successfully!'};
+  },
+  async requestPasswordReset(parent, args, ctx, info) {
+    const user = await ctx.db.query.user({where: {email: args.email}});
+    if (!user) {
+      throw new Error(`no user found with email ${args.email}`);
+    }
+    const randomBytesPromise = promisify(randomBytes);
+    const resetToken = (await randomBytesPromise(20)).toString('hex');
+    const resetTokenExpiry = Date.now() + 1000 * 60  * 60;
+    const res = await ctx.db.mutation.updateUser({
+      where: {email: args.email},
+      data: {resetToken, resetTokenExpiry}
+    });
+
+    const mailRes = await transport.sendMail({
+      from: 'bogdan@me',
+      to: user.email,
+      subject: "Password reset link",
+      html: sendEmail(`Click the <a href="${process.env.FRONTEND_URL}/reset?rt=${resetToken}">link</a> to reset your password. The link will expire in one hour!`)
+    })
+    return {message: 'Check your email for a reset link!'};
+  },
+  async resetPassword(parent, args, ctx, info) {
+    const [user] = await ctx.db.query.users({
+      where: {
+        resetToken: args.resetToken,
+        resetTokenExpiry_gte: Date.now() - 1000 * 60 * 60
+      }
+    });
+    if (!user) {
+      throw new Error('This token is invalid or expired!');
+    } 
+    const password = await bcrypt.hash(args.password, 10);
+    const updatedUser = await ctx.db.mutation.updateUser({
+      where: {email: user.email},
+      data: {
+        password,
+        resetToken: null,
+        resetTokenExpiry: null,
+      }
+    })
+    const token = jwt.sign({userId: updatedUser.id}, process.env.APP_SECRET);
+    ctx.response.cookie('token', token, {
+      httpOnly: true,
+      maxAge
     });
     return user;
   },
--- backend/src/resolvers/Query.js
+++ backend/src/resolvers/Query.js
@@ -1,11 +1,16 @@
 
 const Query = {
   currentUser(parent, args, ctx, info) {
-    console.log('uuu', ctx.request.userId)
     if (!ctx.request.userId) {
       return null;
     }
-    return ctx.db.query.user({where: {id: ctx.request.userId}}, info);
+    return ctx.db.query.user({where: {id: ctx.request.userId}}, info); // get income and expense back
+  },
+  expenses(parent, args, ctx, info) {
+    return ctx.db.query.expenses({where: {userId: args.userId}}, info); // get income and expense back
+  },
+  incomes(parent, args, ctx, info) {
+    return ctx.db.query.incomes({where: {userId: args.userId}}, info); // get income and expense back
   },
 };
 
