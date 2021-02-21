463d0894cdeccfb4a2a256d8adf112ce1f2efac5
bogdan <bogdanbobletec@gmail.com>
2021-02-21 06:12:00 -0500

    add basic expense and income forms

---
+++ frontend/components/Expense.tsx
@@ -0,0 +1,109 @@
+import { useContext } from "react";
+import gql from "graphql-tag";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import { useRouter } from "next/router";
+import useForm from "../lib/useForm";
+import { UserContext } from "./User";
+import Table from "./Table";
+
+const CREATE_EXPENSE_MUTATION = gql`
+  mutation CREATE_EXPENSE_MUTATION(
+    $amount: Float!
+    $category: String!
+    $comments: String
+    $userId: String!
+  ) {
+    createExpense(
+      amount: $amount
+      category: $category
+      comments: $comments
+      userId: $userId
+    ) {
+      id
+      amount
+      category
+      comments
+      userId
+    }
+  }
+`;
+
+const GET_EXPENSES = gql`
+  query getExpenses($userId: String!) {
+    expenses(userId: $userId) {
+      id
+      amount
+      category
+      comments
+    }
+  }
+`;
+
+const Expense: React.FC = () => {
+  const { inputs, handleChange, reset } = useForm();
+  const { amount, category, comments } = inputs;
+  const { id: userId } = useContext(UserContext);
+  const [createExpense] = useMutation(CREATE_EXPENSE_MUTATION);
+  const { data, loading, error } = useQuery(GET_EXPENSES, {
+    variables: { userId },
+  });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createExpense({ variables: { amount, category, comments, userId } });
+
+    if (error) {
+      //todo
+    }
+    reset();
+  };
+
+  const isSubmitDisabled = [amount, category].includes("");
+
+  return (
+    <>
+    <form>
+      <fieldset>
+        <h2>Add expense</h2>
+        <label htmlFor="amount">
+          Amount
+          <input
+            type="number"
+            name="amount"
+            value={amount}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="category">
+          Category
+          <input
+            type="text"
+            name="category"
+            value={category}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="comments">
+          Comments
+          <input
+            type="text"
+            name="comments"
+            value={comments}
+            onChange={handleChange}
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={isSubmitDisabled}
+          onClick={handleSubmit}
+        >
+          Add expense
+        </button>
+      </fieldset>
+    </form>
+    <Table data={data?.expenses}/>
+    </>
+  );
+};
+
+export default Expense;
---
+++ frontend/components/Income.tsx
@@ -0,0 +1,103 @@
+import { useContext } from "react";
+import gql from "graphql-tag";
+import { useMutation, useQuery } from "@apollo/react-hooks";
+import { useRouter } from "next/router";
+import useForm from "../lib/useForm";
+import { UserContext } from "./User";
+import Table from "./Table";
+
+const CREATE_INCOME_MUTATION = gql`
+  mutation CREATE_INCOME_MUTATION(
+    $amount: Float!
+    $category: String!
+    $comments: String
+    $userId: String!
+  ) {
+    createIncome(amount: $amount, category: $category, comments: $comments, userId: $userId) {
+      id
+      amount
+      category
+      comments
+      userId
+    }
+  }
+`;
+
+const GET_INCOMES = gql`
+  query getIncomes($userId: String!) {
+    incomes(userId: $userId) {
+      id
+      amount
+      category
+      comments
+    }
+  }
+`;
+
+const Income: React.FC = () => {
+  const { inputs, handleChange, reset } = useForm();
+  const {id: userId} = useContext(UserContext);
+  const { amount, category, comments } = inputs;
+  const [createIncome] = useMutation(CREATE_INCOME_MUTATION);
+  const { data, loading, error } = useQuery(GET_INCOMES, {
+    variables: { userId },
+  });
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    await createIncome({ variables: { amount, category, comments, userId } });
+
+    if (error) {
+      //todo
+    }
+    reset();
+  };
+
+  const isSubmitDisabled = [amount, category].includes("");
+
+  return (
+    <>
+    <form>
+      <fieldset>
+        <h2>Add Income</h2>
+        <label htmlFor="amount">
+          Amount
+          <input
+            type="number"
+            name="amount"
+            value={amount}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="category">
+          Category
+          <input
+            type="text"
+            name="category"
+            value={category}
+            onChange={handleChange}
+          />
+        </label>
+        <label htmlFor="comments">
+          Comments
+          <input
+            type="text"
+            name="comments"
+            value={comments}
+            onChange={handleChange}
+          />
+        </label>
+        <button
+          type="submit"
+          disabled={isSubmitDisabled}
+          onClick={handleSubmit}
+        >
+          Add income
+        </button>
+      </fieldset>
+    </form>
+    <Table data={data?.incomes}/>
+    </>
+  );
+};
+
+export default Income;
